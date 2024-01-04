68f28bd2669b20f5a2567f22899c4946d50eb6f3
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2024-01-04 14:58:42 +0000

    adds dark theme and toggle switch

--- App.jsx
+++ App.jsx
@@ -1,36 +1,81 @@
-import { StyleSheet, Image, } from 'react-native';
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { useState, useContext } from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+  useTheme,
+} from "@react-navigation/native";
 import Constants from "expo-constants";
-import{ PaperProvider, Appbar, } from "react-native-paper";
+import {
+  PaperProvider,
+  Appbar,
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+  Switch,
+  Badge,
+} from "react-native-paper";
 import Home from "./components/home";
-import PlanTrip from './components/plan-trip';
+import PlanTrip from "./components/plan-trip";
+import merge from "deepmerge";
 
 const Tab = createMaterialTopTabNavigator();
 
-
 export default function App() {
-  return (
-    <PaperProvider>
-    <>
-    <Appbar style={{ marginTop: Constants.statusBarHeight }}>
-      <Appbar.Content title="Travel Chum" />
-      <Image
-        style={styles.Image}
-        source={require("./assets/Travel-Chum-Logo.png")}
-      />
-    </Appbar>
-  </>
+  const { LightTheme, DarkTheme } = adaptNavigationTheme({
+    reactNavigationLight: NavigationDefaultTheme,
+    reactNavigationDark: NavigationDarkTheme,
+  });
 
-    <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Trip Planner" component={PlanTrip} />
-    </Tab.Navigator>
-    </NavigationContainer>
+  const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+  const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
+
+  const [isThemeDark, setIsThemeDark] = useState(false);
+
+  let theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark]
+  );
+  return (
+    <PaperProvider theme={theme}>
+      <>
+        <Appbar style={{ marginTop: Constants.statusBarHeight }}>
+          <Appbar.Content title="Travel Chum" />
+          <Image
+            style={styles.Image}
+            source={require("./assets/Travel-Chum-Logo.png")}
+          />
+
+          <Appbar.Action icon="theme-light-dark" />
+          <Switch
+            color={"purple"}
+            value={isThemeDark}
+            onValueChange={toggleTheme}
+          />
+          <Badge>dark</Badge>
+
+          <Appbar.Action icon="lightbulb-off-outline" />
+        </Appbar>
+      </>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Trip Planner" component={PlanTrip} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
-  )
-    
+  );
 }
 
 const styles = StyleSheet.create({
@@ -48,7 +93,7 @@ const styles = StyleSheet.create({
     height: "100%",
     width: "15%",
     alignSelf: "center",
-   
+
     resizeMode: "contain",
   },
 });
