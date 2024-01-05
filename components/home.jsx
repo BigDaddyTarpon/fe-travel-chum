342dda5fdb77c462ab6e04e294144187a37bf870
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2024-01-05 11:43:27 +0000

    add provisional welcome message to home

--- components/home.jsx
+++ components/home.jsx
@@ -1,18 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+
+  Dialog,
+} from "react-native-paper";
+import { StyleSheet, } from "react-native";
+
+
+
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Home Page</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Dialog visible={true}>
+        <Dialog.Title>Welcome to Travel Chum.</Dialog.Title>
+        <Dialog.Title style={{ fontSize: 20 }}>
+        
+          Swipe sideways or click a tab at the top to navigate.
+        </Dialog.Title>
+
+        <Dialog.Title style={{ fontSize: 20 }}>
+        You can select Darkmode with the switch at the top right.
+        </Dialog.Title>
+        <Dialog.Title style={{ fontSize: 20 }}>
+          login, with the icon top right.
+        </Dialog.Title>
+        <Dialog.Title style={{ fontSize: 20 }}>
+          You can enter details on 'plan a trip' to see the trip dispayed on the
+          map.
+        </Dialog.Title>
+
+
+      </Dialog>
+    </>
   );
 }
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  });
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
+  },
+});
