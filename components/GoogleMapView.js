05e7e47af9cf1304b238961b5694bb4a007126ec
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2024-01-07 11:23:14 +0000

    removes map-night-style-object from main GoogleMapView file, making it an import

--- components/GoogleMapView.js
+++ components/GoogleMapView.js
@@ -2,175 +2,17 @@ import { View, Dimensions } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import MapView, { PROVIDER_GOOGLE, Polyline } from "react-native-maps";
 import { UserLocationContext } from "./Contexts";
-import { PreferencesContext } from '../PreferencesContext';
+import { PreferencesContext } from "../PreferencesContext";
+import { MapStyleNight } from "./map-night-style-object.js";
 
-const mapStyle = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#242f3e"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#746855"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#242f3e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.locality",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#d59563"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#d59563"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#263c3f"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#6b9a76"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#38414e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#212a37"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9ca5b3"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#746855"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#1f2835"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#f3d19c"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#2f3948"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#d59563"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#17263c"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#515c6d"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#17263c"
-      }
-    ]
-  }
-]
-export default function GoogleMapView({polylineCoordinates}) {
-  const preferences=useContext(PreferencesContext)
+const mapStyle = MapStyleNight;
+
+export default function GoogleMapView({ polylineCoordinates }) {
+  const preferences = useContext(PreferencesContext);
   const [mapRegion, setMapRegion] = useState([]);
-  const [mapView, setMapView] = useState()
-  const [forceToggle, setForceToggle] = useState(!preferences.isThemeDark)
-  
+  const [mapView, setMapView] = useState();
+  const [forceToggle, setForceToggle] = useState(!preferences.isThemeDark);
+
   const { location, setUserLocation } = useContext(UserLocationContext);
 
   useEffect(() => {
@@ -186,7 +28,7 @@ export default function GoogleMapView({polylineCoordinates}) {
 
   return (
     <View
-    style={{display: "flex", flexDirection: "column", alignItems: "center" }}
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <MapView
         style={{
@@ -200,16 +42,17 @@ export default function GoogleMapView({polylineCoordinates}) {
         showsMyLocationButton={true}
         region={mapRegion}
         customMapStyle={preferences.isThemeDark ? mapStyle : []}
-       
       >
-      {polylineCoordinates ? 
-      <Polyline
-        coordinates={polylineCoordinates}
-        strokeWidth={3}
-        strokeColor="blue"
-      /> : <></>}
+        {polylineCoordinates ? (
+          <Polyline
+            coordinates={polylineCoordinates}
+            strokeWidth={3}
+            strokeColor="blue"
+          />
+        ) : (
+          <></>
+        )}
       </MapView>
-      
     </View>
   );
 }
